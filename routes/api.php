e9f33f90734a2897693e3597360f8b30f889e63b
memonbayu11 <you@example.com>
2021-10-12 22:01:34 +0700

    challenge-author

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 
 Route::get('/me', [AuthController::class, 'me']);
 Route::resource('books', BookController::class);
+Route::resource('authors', AuthorController::class);
